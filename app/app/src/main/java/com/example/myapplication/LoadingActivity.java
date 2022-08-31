1dca599eaa927b4c454684a789d403985362accf
NoahMeissner <108337767+NoahMeissner@users.noreply.github.com>
2022-08-31 11:21:04 +0200

    Loading Page
    Wenn man App startet um Inhalte zu laden

--- app/app/src/main/java/com/example/myapplication/LoadingActivity.java
+++ app/app/src/main/java/com/example/myapplication/LoadingActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoadingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initUi();
+    }
+
+    private void initUi() {
         setContentView(R.layout.activity_loading);
 
     }
+
 }

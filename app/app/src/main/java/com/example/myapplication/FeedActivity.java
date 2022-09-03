347481c440cd98901bc5a0d033c9c3381ad2b218
Max <civnopes@gmail.com>
2022-09-03 15:48:30 +0200

    Added titleBar custom view

--- app/app/src/main/java/com/example/myapplication/FeedActivity.java
+++ app/app/src/main/java/com/example/myapplication/FeedActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
@@ -21,24 +23,16 @@ public class FeedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-        this.titleDate = findViewById(R.id.feed_title_date);
-        this.titleDayOfWeek = findViewById(R.id.feed_title_day_of_week);
+        // Title-bar
+        setSupportActionBar(findViewById(R.id.toolbar));
+        CollapsingToolbarLayout toolBarLayout = findViewById(R.id.toolbar_layout);
+        toolBarLayout.setContentScrimColor(getColor(R.color.md_theme_light_background));
 
-        LocalDateTime now = LocalDateTime.now();
-        this.titleDate.setText(getTitleDate(now));
-        this.titleDayOfWeek.setText(getDayOfWeek(now));
-    }
-
-    // Returns the title for the feed in the default locale
-    // Format: 20. August
-    private String getDayOfWeek(LocalDateTime now) {
-        return now.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault());
-    }
-
-    // Returns the day of the week in the default locale
-    // Format: Samstag
-    private String getTitleDate(LocalDateTime now) {
-        return DateTimeFormatter.ofPattern("dd. ").format(now) +
-                now.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault());
+//        this.titleDate = findViewById(R.id.feed_title_date);
+//        this.titleDayOfWeek = findViewById(R.id.feed_title_day_of_week);
+//
+//        LocalDateTime now = LocalDateTime.now();
+//        this.titleDate.setText(getTitleDate(now));
+//        this.titleDayOfWeek.setText(getDayOfWeek(now));
     }
 }
---
+++ app/app/src/main/java/com/example/myapplication/view/TitleBar.java
@@ -0,0 +1,78 @@
+package com.example.myapplication.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewStub;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.example.myapplication.R;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class TitleBar extends CoordinatorLayout {
+
+    private int pageContentResourceId;
+
+    public TitleBar(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        // Read and initialize provided attributes
+        initializeAttributes(context, attrs);
+
+        // Inflate title-bar
+        View.inflate(context, R.layout.title_bar, this);
+
+        // Initialize content
+        setTitle();
+        setPageContent();
+    }
+
+    private void initializeAttributes(@NonNull Context context, @Nullable AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.TitleBar,
+                0, 0);
+        try {
+            pageContentResourceId = a.getResourceId(
+                    R.styleable.TitleBar_pageContentView, 0
+            );
+        } finally {
+            a.recycle();
+        }
+    }
+
+    private void setTitle() {
+        CollapsingToolbarLayout toolbarLayout = this.findViewById(R.id.toolbar_layout);
+        toolbarLayout.setTitle(getDateTitle(LocalDateTime.now()));
+    }
+
+    private void setPageContent() {
+        if (this.pageContentResourceId != 0) {
+            ViewStub pageContent = (ViewStub) this.findViewById(R.id.page_content);
+            pageContent.setLayoutResource(pageContentResourceId);
+            pageContent.inflate();
+        }
+    }
+
+    // Returns the day of the week in the default locale
+    // Format: Samstag
+    private String getDateTitle(LocalDateTime now) {
+        return DateTimeFormatter.ofPattern("dd. ").format(now) +
+                now.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault());
+    }
+
+//    // Returns the title for the feed in the default locale
+//    // Format: 20. August
+//    private String getDayOfWeek(LocalDateTime now) {
+//        return now.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault());
+//    }
+}

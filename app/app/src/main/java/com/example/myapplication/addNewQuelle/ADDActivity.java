46935ea9a3a28cef66a883839e8e12063d5cd9e1
NoahMeissner <108337767+NoahMeissner@users.noreply.github.com>
2022-09-04 14:13:56 +0200

    commit

--- app/app/src/main/java/com/example/myapplication/addNewQuelle/ADDActivity.java
+++ app/app/src/main/java/com/example/myapplication/addNewQuelle/ADDActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import com.example.myapplication.R;
 import com.example.myapplication.addNewQuelle.adapter.AdapterListAddActivity;
@@ -30,23 +31,40 @@ public class ADDActivity extends AppCompatActivity implements AdapterListAddActi
         initUI();
     }
 
+    // in this method all elements are initialized
     private void initUI() {
         setContentView(R.layout.activity_addactivity);
-        RecyclerView recylerViewSM = findViewById(R.id.recyclerViewQuellenSM);
-        RecyclerView recylerViewNP = findViewById(R.id.recyclerViewQuellenNP);
-        RecyclerView recyclerViewIn = findViewById(R.id.recyclerViewQuellenIn);
         initHashMap();
         editpictures();
         initQuellen();
+        declareRecyclerView();
+        initButton();
+    }
+
+    // This Method initialise the Buttons to close the Actitivity and show the Information Fragement
+    private void initButton() {
+        ImageButton buttonInformation = findViewById(R.id.informationButton);
+        ImageButton backButton = findViewById(R.id.imagebackButton);
+        buttonInformation.setOnClickListener(view -> {
+            InformationenFragement informationenFragement = new InformationenFragement();
+            informationenFragement.show(getSupportFragmentManager(),"openInformationFragement");
+        });
+        backButton.setOnClickListener(view -> {
+            //@TODO zurück zum Home FEED
+        });
+    }
+
+    // in this method the recycler view is initialized and passed to the initRecyclerView method
+    private void declareRecyclerView(){
+        RecyclerView recylerViewSM = findViewById(R.id.recyclerViewQuellenSM);
+        RecyclerView recylerViewNP = findViewById(R.id.recyclerViewQuellenNP);
+        RecyclerView recyclerViewIn = findViewById(R.id.recyclerViewQuellenIn);
         initRecyclerView(recylerViewNP,newsArrayList);
         initRecyclerView(recylerViewSM,socialMediaQuellenArrayList);
         initRecyclerView(recyclerViewIn,interestsArrayList);
     }
 
-
-
-
-
+    //In this method, the hashmap is initialized in which the category and the image are located in order to assign them quickly
     @SuppressLint("UseCompatLoadingForDrawables")
     private void initHashMap(){
         hashMap.put(new String[]{String.valueOf(Categories.SocialMedia), "Twitter"},getDrawable(R.drawable.twitter_icon));
@@ -61,6 +79,7 @@ public class ADDActivity extends AppCompatActivity implements AdapterListAddActi
         hashMap.put(new String[]{String.valueOf(Categories.Interessen), "Sport"},getDrawable(R.drawable.sport));
     }
 
+    // With this method you can easily edit the images of the buttons
     @SuppressLint("ResourceAsColor")
     private void editpictures(){
         for(String[] s: hashMap.keySet()){
@@ -70,6 +89,7 @@ public class ADDActivity extends AppCompatActivity implements AdapterListAddActi
         }
     }
 
+    //in this method the source objects are created
     @SuppressLint("UseCompatLoadingForDrawables")
     private void initQuellen(){
         for(String[]s : hashMap.keySet()){
@@ -84,18 +104,19 @@ public class ADDActivity extends AppCompatActivity implements AdapterListAddActi
             }
 
         }
-            socialMediaQuellenArrayList.add(new Quellen("",getDrawable(R.drawable.add)));
-            newsArrayList.add(new Quellen("",getDrawable(R.drawable.add)));
-            interestsArrayList.add(new Quellen("",getDrawable(R.drawable.add)));
+            socialMediaQuellenArrayList.add(new Quellen(String.valueOf(Categories.ADDButton),getDrawable(R.drawable.add)));
+            newsArrayList.add(new Quellen(String.valueOf(Categories.ADDButton),getDrawable(R.drawable.add)));
+            interestsArrayList.add(new Quellen(String.valueOf(Categories.ADDButton),getDrawable(R.drawable.add)));
     }
 
+    // In this method, depending on a RecyclerView, the recycler view is processed and connected to the adapter
     private void initRecyclerView(RecyclerView recyclerView, ArrayList<Quellen> arrayList) {
-        // RecylerView
         recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
         AdapterListAddActivity recyclerViewAdapter = new AdapterListAddActivity(this,arrayList);
         recyclerView.setAdapter(recyclerViewAdapter);
     }
 
+    //this method is inherited from the onclick listener here and using it we can open the fragment depending on the button clicked
     @Override
     public void onItemClick(Quellen quellen) {
         EditQuellenFragement editQuellenFragement = new EditQuellenFragement();

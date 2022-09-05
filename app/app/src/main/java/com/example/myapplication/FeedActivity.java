db083c154bb3ddfcf6efaeef5aae496b6991b274
Max <civnopes@gmail.com>
2022-09-05 12:01:41 +0200

    Cleanup

--- app/app/src/main/java/com/example/myapplication/FeedActivity.java
+++ app/app/src/main/java/com/example/myapplication/FeedActivity.java
@@ -15,9 +15,6 @@ import java.util.Locale;
 
 public class FeedActivity extends AppCompatActivity {
 
-    private TextView titleDate; // Format: 20. August
-    private TextView titleDayOfWeek; // Format: Samstag
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,14 +22,5 @@ public class FeedActivity extends AppCompatActivity {
 
         // Title-bar
         setSupportActionBar(findViewById(R.id.toolbar));
-        CollapsingToolbarLayout toolBarLayout = findViewById(R.id.toolbar_layout);
-        toolBarLayout.setContentScrimColor(getColor(R.color.md_theme_light_background));
-
-//        this.titleDate = findViewById(R.id.feed_title_date);
-//        this.titleDayOfWeek = findViewById(R.id.feed_title_day_of_week);
-//
-//        LocalDateTime now = LocalDateTime.now();
-//        this.titleDate.setText(getTitleDate(now));
-//        this.titleDayOfWeek.setText(getDayOfWeek(now));
     }
 }

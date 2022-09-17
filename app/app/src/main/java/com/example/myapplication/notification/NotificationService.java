4f166878d1166b73f768f60423b418e3e3c9d7c8
NoahMeissner <108337767+NoahMeissner@users.noreply.github.com>
2022-09-17 07:45:21 +0200

    w

--- app/app/src/main/java/com/example/myapplication/notification/NotificationService.java
+++ app/app/src/main/java/com/example/myapplication/notification/NotificationService.java
@@ -1,12 +1,35 @@
 package com.example.myapplication.notification;
 
-public class NotificationService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+import java.time.LocalDateTime;
+
+public class NotificationService extends Service {
+
+    LocalDateTime time = LocalDateTime.now();
 
 
     public NotificationService(){
 
     }
 
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 }
